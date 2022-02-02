820ead24f7e0ad3fe52bdc5878ed2829b04facb0
cmondi18 <disivolap@gmail.com>
2022-02-02 19:24:11 +0400

    feat: add not null attr migration

---
+++ db/migrate/20220202151920_add_models_null_constraint.rb
@@ -0,0 +1,11 @@
+class AddModelsNullConstraint < ActiveRecord::Migration[6.1]
+  def change
+    change_column_null(:tests, :title, false)
+    change_column_null(:tests, :level, false)
+    change_column_null(:categories, :title, false)
+    change_column_null(:questions, :body, false)
+    change_column_null(:answers, :user_id, false)
+    change_column_null(:answers, :question_id, false)
+    change_column_null(:users, :username, false)
+  end
+end
