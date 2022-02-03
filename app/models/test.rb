62d660262e2f5d966e9f057475b1905a05e65035
cmondi18 <disivolap@gmail.com>
2022-02-03 22:40:05 +0400

    fix: after review

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,8 @@
 class Test < ApplicationRecord
   def self.test_titles_by_category(category)
-    Test.joins("JOIN categories ON tests.category_id = categories.id WHERE categories.title = '#{category}'")
-        .pluck(:title).sort.reverse
+    Test.joins('JOIN categories ON tests.category_id = categories.id')
+        .where('categories.title = ?', category)
+        .order(title: :desc)
+        .pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   def tests_by_level(level)
-    Test.joins("JOIN user_tests ON tests.id = user_tests.test_id WHERE level = #{level} AND user_tests.user_id = #{self.id}").first
+    Test.joins("JOIN user_tests ON tests.id = user_tests.test_id")
+        .where('level = ?', level).where('user_tests.user_id = ?', id)
   end
 end
