3378d86e54b3e920c9914507fed12fca76d3695e
cmondi18 <disivolap@gmail.com>
2022-02-03 11:09:50 +0400

    fix: add foreign key

--- db/migrate/20220202151531_create_answers.rb
+++ db/migrate/20220202151531_create_answers.rb
@@ -3,7 +3,7 @@ class CreateAnswers < ActiveRecord::Migration[6.1]
     create_table :answers do |t|
       t.string :body
       t.boolean :correct, default: false
-      t.references :question
+      t.references :question, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -50,4 +50,7 @@ ActiveRecord::Schema.define(version: 2022_02_02_151605) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "answers", "questions"
+  add_foreign_key "questions", "tests"
+  add_foreign_key "tests", "categories"
 end
