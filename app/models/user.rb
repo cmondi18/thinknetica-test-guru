ddea89b00b1186606eaf9caab37a967e722f5e1e
cmondi18 <disivolap@gmail.com>
2022-02-03 20:06:32 +0400

    feat: add search tests by level method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def tests_by_level(level)
+    Test.joins("JOIN user_tests ON tests.id = user_tests.test_id WHERE level = #{level} AND user_tests.user_id = #{self.id}").first
+  end
 end
