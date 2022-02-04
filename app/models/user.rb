3db47cf7ff595166ce602ad1ea53a7b2d70c41ff
cmondi18 <disivolap@gmail.com>
2022-02-04 20:48:27 +0400

    feat: add author and created tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   has_many :users_tests
   has_many :tests, through: :users_tests
+  has_many :created_tests, foreign_key: :user_id, class_name: 'Test'
 
   def tests_by_level(level)
-    Test.joins("JOIN user_tests ON tests.id = user_tests.test_id")
-        .where('level = ?', level).where('user_id = ?', id)
+    Test.joins("JOIN users_tests ON tests.id = users_tests.test_id")
+        .where('level = ?', level).where('users_tests.user_id = ?', id)
   end
 end
