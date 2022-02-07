65295cb1cdb66dfb689788f896019f2ef09c1824
cmondi18 <disivolap@gmail.com>
2022-02-07 22:58:30 +0400

    fix: after review

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,8 @@
 class QuestionsController < ApplicationController
-  before_action :test, only: %w[index create]
-  before_action :question, only: %w[show destroy]
+  before_action :set_test, only: %w[index create]
+  before_action :set_question, only: %w[show destroy]
   # used to test via requests (ex. Postman)
-  # skip_before_action :verify_authenticity_token, only: :destroy
+  skip_before_action :verify_authenticity_token, only: :destroy
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
@@ -15,11 +15,19 @@ class QuestionsController < ApplicationController
   def create
     @question = @test.questions.new(question_params)
 
-    redirect_to test_questions_path if @question.save
+    if @question.save
+      redirect_to test_questions_path
+    else
+      render plain 'Something went wrong'
+    end
   end
 
   def destroy
-    render plain: 'Question was deleted' if @question.destroy
+    if @question.destroy
+      render plain: 'Question was deleted'
+    else
+      render plain 'Something went wrong'
+    end
   end
 
   private
@@ -28,11 +36,11 @@ class QuestionsController < ApplicationController
     params.require(:question).permit(:body)
   end
 
-  def test
+  def set_test
     @test = Test.find(params[:test_id])
   end
 
-  def question
+  def set_question
     @question = Question.find(params[:id])
   end
 
