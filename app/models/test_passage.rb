ec0258960aa89774626a3db589ec27c9e36ea16f
cmondi18 <disivolap@gmail.com>
2022-02-11 14:19:25 +0300

    feat: change result page

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -1,21 +1,29 @@
 class TestPassage < ApplicationRecord
+  SUCCESSFUL_RATE = 85
+
   belongs_to :user
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
+  before_update :before_update_set_next_question
 
   def completed?
     current_question.nil?
   end
 
   def accept!(answer_ids)
-    if correct_answer?(answer_ids)
-      self.correct_questions += 1
-    end
+    self.correct_questions += 1 if correct_answer?(answer_ids)
 
-    self.current_question = next_question
     save!
+  end
+
+  def result
+    (self.correct_questions * 100) / self.test.questions.count
+  end
+
+  def success?
+    result > SUCCESSFUL_RATE
   end
 
   private
@@ -25,14 +33,14 @@ class TestPassage < ApplicationRecord
   end
 
   def correct_answer?(answer_ids)
-    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
+    answer_ids && answer_ids.map(&:to_i).sort == correct_answers.ids.sort
   end
 
   def correct_answers
     current_question.answers.correct
   end
 
-  def next_question
-    test.questions.order(:id).where('id > ?', current_question.id).first
+  def before_update_set_next_question
+    self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
   end
 end
