ca9a5e0a58a458fc11e3a0cdb32e3785c0c20f17
cmondi18 <disivolap@gmail.com>
2022-02-13 12:05:01 +0300

    feat: add flash helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
-  def new
+  include FlashHelper
 
-  end
+  def new; end
 
   def create
     user = User.find_by(email: params[:email])
@@ -11,7 +11,8 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to tests_path
     else
-      flash.now[:alert] = 'Are you a Guru? Verify your email and password, please'
+      register_flash_now_message(:alert,
+                                 'Are you a Guru? Verify your email and password, please')
       render :new
     end
   end
