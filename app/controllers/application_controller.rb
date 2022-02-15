44cf94bf6260c8cab163a5ea79bf7e7d727deba9
cmondi18 <disivolap@gmail.com>
2022-02-15 16:28:46 +0300

    feat: add devises controllers to redirect and flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-
-  def after_sign_in_path_for(current_user)
-    if current_user.is_a?(Admin)
-      admin_tests_path
-    else
-      tests_path
-    end
-  end
 end
