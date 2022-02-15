d3f4fd21487278c8bb9837fc408dfab566e2ea1f
cmondi18 <disivolap@gmail.com>
2022-02-15 09:08:13 +0300

    feat: add admin

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,44 +1,8 @@
 class TestsController < ApplicationController
-  before_action :set_test, only: %i[show edit update destroy start]
+  before_action :set_test, only: %i[start]
 
   def index
     @tests = Test.all
-  end
-
-  def show
-    @questions = @test.questions
-  end
-
-  def new
-    @test = current_user.created_tests.new
-  end
-
-  def edit; end
-
-  def create
-    @test = current_user.created_tests.new(test_params)
-
-    if @test.save
-      redirect_to @test
-    else
-      render :new
-    end
-  end
-
-  def update
-    if @test.update(test_params)
-      redirect_to @test
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    if @test.destroy
-      redirect_to @test
-    else
-      render :show
-    end
   end
 
   def start
@@ -47,10 +11,6 @@ class TestsController < ApplicationController
   end
 
   private
-
-  def test_params
-    params.require(:test).permit(:title, :level, :category_id)
-  end
 
   def set_test
     @test = Test.find(params[:id])
